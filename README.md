57cbb670a06a0f2a3ff9251d6711aa45615356b5
Victor David Santos <victordavidsantos@gmail.com>
2014-04-06 11:01:59 -0300

    Versão inicial da biblioteca. Jogo mínimo para testes da biblioteca.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-aventura-do-saber
+Aventura do Saber
 =================
 
 Um framework para games e um jogo educativo feitos em Ruby, com base na biblioteca Gosu.
---
+++ src/game.rb
@@ -0,0 +1,59 @@
+require 'gosu'
+require_relative 'lib/game_object'
+require_relative 'lib/forms'
+include AGL
+
+class MyGame < Gosu::Window
+	def initialize
+		super 800, 600, false
+		self.caption = "Game Test"
+		
+		Game.initialize self
+		
+		@man = GameObject.new 200, 200, 40, 115, :sprite1, Vector.new(-10, -5), 3, 2
+		@man_indices = [0, 1, 0, 2, 0, 1, 0, 2, 3, 4, 3, 5, 3, 4, 3, 5]
+		@man_interval = 9
+		
+		@obst = [
+			Block.new(100, 500, 600, 1, false),
+			Block.new(99, 100, 1, 400, false),
+			Block.new(500, 100, 1, 400, false)
+		]
+		
+		@font = Gosu::Font.new self, "data/font/Ubuntu-L.ttf", 20
+		@button = Button.new(600, 100, @font, "Restart", :btn1) {
+			@man.speed.x = 0; @man.speed.y = 0
+			@man.x = 200; @man.y = 200
+		}
+	end
+	
+	def needs_cursor?
+		true
+	end
+	
+	def update
+		KB.update
+		Mouse.update
+		@button.update
+		
+		@man.animate @man_indices, @man_interval
+		
+		forces = Vector.new 0, 0
+		forces.x -= 0.3 if KB.key_down? Gosu::KbLeft
+		forces.x += 0.3 if KB.key_down? Gosu::KbRight
+		forces.y -= 15 if KB.key_down? Gosu::KbUp and @man.bottom
+		@man.move forces, @obst, []
+	end
+	
+	def draw
+		draw_quad 100, 100, 0xffffffff,
+		          500, 100, 0xffffffff,
+		          500, 500, 0xffffffff,
+		          100, 500, 0xffffffff, 0
+		@man.draw
+		@button.draw
+	end
+end
+
+game = MyGame.new
+game.show
