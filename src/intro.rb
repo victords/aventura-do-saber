930862eef1c6a2e45b38ec3726e29c777711a273
Victor David Santos <victordavidsantos@gmail.com>
2014-10-05 12:05:24 -0300

    Ajustes na tela de apresentação.

--- src/intro.rb
+++ src/intro.rb
@@ -11,14 +11,17 @@ class Intro
 			end
 		@scenes = []
 		Dir["data/img/bg/#{type}Intro*"].sort.each do |f|
-			@scenes << Gosu::Image.new(G.win, f, false)
+			id = f.split('/')[2..3].join('_').chomp(".png")
+			@scenes << Sprite.new(-100, -75, id)
 		end
 		@texts = []
 		File.open("data/text/#{type}.txt").each do |l|
 			s = l.chomp.gsub("\\n", "\n")
 			@texts << XText.new(s, 10, 600 - s.split("\n").size * 36, 0, true)
 		end
+		@hint = XText.new "Pressione 'A' para continuar...", 10, 5, 0x00ffff
 		
+		@scale = 1
 		@cur_scene = 0
 		@rect_alpha = 255
 		@transition = 0
@@ -26,6 +29,7 @@ class Intro
 	
 	def update
 		@texts.each { |t| t.update_alpha }
+		@hint.update_alpha
 		if @transition == 0
 			@rect_alpha -= 5
 			if @rect_alpha == 0
@@ -40,19 +44,28 @@ class Intro
 					return true
 				else
 					@transition = 0
+					@scale = 1
+					@hint.alpha = 0
 				end
 			end
 		else
 			d_x = (100 - @texts[@cur_scene].x) / 50.0
 			@texts[@cur_scene].x += d_x if d_x.round(2) > 0
+			if @scale > 0.8
+				@scale *= 0.9995
+				@scenes[@cur_scene].x = (800 - (@scale * 1000)) / 2
+				@scenes[@cur_scene].y = (600 - (@scale * 750)) / 2
+				@hint.fade_in if @scale <= 0.8
+			end
 			@transition = 1 if KB.key_pressed? Gosu::KbA
 		end
 		false
 	end
 	
 	def draw
-		@scenes[@cur_scene].draw 0, 0, 0
+		@scenes[@cur_scene].draw nil, @scale, @scale
 		@texts[@cur_scene].draw
+		@hint.draw
 		if @transition
 			color = (@rect_alpha << 24)
 			G.win.draw_quad 0, 0, color,
